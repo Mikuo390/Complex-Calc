a056e47f4136a1985e00a43ffbfcba0597564ff8
Mikuo390 <72961325+Mikuo390@users.noreply.github.com>
2021-10-11 10:53:51 +0800

    Create AbsCalc.java

---
+++ src/com/yjh/complex/utils/impl/AbsCalc.java
@@ -0,0 +1,22 @@
+package com.yjh.complex.utils.impl;
+
+import com.yjh.complex.domain.Complex;
+import com.yjh.complex.ui.CalcUI;
+import com.yjh.complex.utils.ICalcRole;
+
+public class AbsCalc implements ICalcRole{
+	private static String r1 = CalcUI.realpartText1.getText();
+	private static String i1 = CalcUI.imagpartText1.getText();
+	
+	private static Complex c1 = new Complex(Double.valueOf(r1),Double.valueOf(i1));
+	private static Complex result = new Complex();
+	
+	@Override
+	public void calcComplex() {
+		result.setRealpart(Math.pow(c1.getRealpart(), 2));
+		result.setImagpart(Math.pow(c1.getImagpart(), 2));
+		
+		CalcUI.realpartResult.setText(result.getRealpart()+"");
+		CalcUI.imagpartResult.setText(result.getImagpart()+"");
+	}
+}
